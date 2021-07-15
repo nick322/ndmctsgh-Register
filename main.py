9c9fa674f83539184557621eb56993f57399f44e
nick80322 <nick80322@gmail.com>
2021-07-15 14:14:50 +0800

    adjust url query

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import time
 import pytesseract
+import urllib
 
 load_dotenv(encoding='utf-8')
 doctorName = os.getenv('DOCTOR_NAME')
@@ -12,49 +13,56 @@ UserROCID = os.getenv('USER_ROCID')
 # 使用 Chrome 的 WebDriver
 browser = webdriver.Chrome()
 
-browser.get(
-    'https://www2.ndmctsgh.edu.tw/NewWebReg/Register/Doctors?pos=B&DeptCode=110&DeptGroup=1')
+while True: 
+    browser.get(
+        'https://www2.ndmctsgh.edu.tw/NewWebReg/Register/Doctors?pos=B&DeptCode=110&DeptGroup=1')
 
-# get var _schedules
-_schedules = browser.execute_script('return _schedules')
+    # get var _schedules
+    _schedules = browser.execute_script('return _schedules')
 
-# print(_schedules)
-# print(type(_schedules))
-RowId = []
-for week in _schedules:
-    # print(week)
-    # print(type(week))
+    # print(_schedules)
+    # print(type(_schedules))
+    RowId = []
+    for week in _schedules:
+        # print(week)
+        # print(type(week))
 
-    for Dates in week['Dates']:
-        # print(Dates)
-        # print(type(Dates))
+        for Dates in week['Dates']:
+            # print(Dates)
+            # print(type(Dates))
 
-        if bool(Dates['SegTimes']):
+            if bool(Dates['SegTimes']):
 
-            for Doctor in Dates['SegTimes'][0]['Doctors']:
+                for Doctor in Dates['SegTimes'][0]['Doctors']:
 
-                if Doctor['Name'] == doctorName and bool(Doctor['RowId']):
-                    RowId.append(Doctor['RowId'])
-                    print(Doctor)
-                    # print(type(Doctor))
-                    # print('------------------------------')
-                    print(f'\n')
+                    if Doctor['Name'] == doctorName and bool(Doctor['RowId']):
+                        RowId.append(Doctor['RowId'])
+                        print(Doctor)
+                        # print(type(Doctor))
+                        # print('------------------------------')
+                        print(f'\n')
 
-print('預約[]=> ', RowId)
+    print('預約[]=> ', RowId)
+    if bool(RowId):
+        break
+
+
 browser.get('https://www2.ndmctsgh.edu.tw/NewWebReg/')
-next_button = browser.find_element_by_xpath('/html/body/div/div/p[11]/a[1]')
+next_button = browser.find_element_by_xpath(
+    '/html/body/div/div/p[11]/a[1]')
 next_button.click()
 time.sleep(1)
 for id in RowId:
+    id = urllib.parse.urlencode({'id': id})
     browser.get(
-        'https://www2.ndmctsgh.edu.tw/NewWebReg/Register/RegData?id=' + id)
-    print('https://www2.ndmctsgh.edu.tw/NewWebReg/Register/RegData?id=' + id)
+        'https://www2.ndmctsgh.edu.tw/NewWebReg/Register/RegData?' + id)
+    print('https://www2.ndmctsgh.edu.tw/NewWebReg/Register/RegData?' + id)
 
     with open('filename.jpg', 'wb') as file:
         file.write(browser.find_element_by_xpath(
             '/html/body/div/div[2]/div/form/div[3]/div[2]/img').screenshot_as_png)
 
-    img = Image.open(r'C:\chrome\filename.jpg')
+    img = Image.open(r'.\filename.jpg')
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     config = '--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789'
     text = pytesseract.image_to_string(img, config=config)
